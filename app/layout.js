090a0dcb810f7c29c3505653c8912294cad8a7ef
OtecVsevolod <aaaseva68@gmail.com>
2025-11-15 21:45:17 +0100

    new layot

--- app/layout.js
+++ app/layout.js
@@ -6,14 +6,14 @@ import { Toaster } from "react-hot-toast";
 const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
 
 export const metadata = {
-  title: "QuickCart - GreatStack",
-  description: "E-Commerce with Next.js ",
+  title: "Kazochka Shop — Twój Market Online",
+  description: "Kazochka Shop to nowoczesny marketplace, w którym znajdziesz elektronikę, akcesoria i wiele innych produktów w najlepszych cenach.",
 };
 
 export default function RootLayout({ children }) {
   return (
-      <html lang="en">
-        <body className={`${outfit.className} antialiased text-gray-700`} >
+      <html lang="pl">
+        <body className={`${outfit.className} antialiased text-gray-700`}>
           <Toaster />
           <AppContextProvider>
             {children}
